61a85df2c9a3ef1c4eb006d5f539ffc9e64240fb
arjankrosaa <149768322+arjankrosaa@users.noreply.github.com>
2023-11-18 15:43:48 +0100

    kalkulatori

---
+++ index.js
@@ -0,0 +1,7 @@
+//llogaritja e dimensioneve tek kalkulatori
+
+function llogaritDimensionet(value){
+    var rezultati;
+    rezultati= 300 * value
+    document.getElementById("result").value=rezultati;
+}
