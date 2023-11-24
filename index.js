b199b26c628f1f6416bde83d05dedb59507dbc53
arjankrosaa <149768322+arjankrosaa@users.noreply.github.com>
2023-11-24 11:47:53 +0100

    proj

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ function llogaritDimensionet(value){
 function NewTab() {
             window.open("Projektet.html" , "_self");
         }
+
+
+
+        const butoni=document.querySelectorAll(".button");
+        const image= document.querySelectorAll(".image");
+        
+        // kushtezimi per filtrim te kategorive
+        
+        for(i=0;i<butoni.length;i++)
+        {
+            butoni[i].addEventListener("click", (e)=>{
+                e.preventDefault();
+                const filter= e.target.dataset.filter;
+                image.forEach((image)=>{
+                    if(filter=== "tegjitha")
+                    {
+                        image.style.display="block";
+                    }
+                    else {
+                        if(image.classList.contains(filter)) 
+                            {
+                            image.style.display="block";
+                            }
+                        else{
+                            image.style.display="none";
+                        }    
+                    }     
+                }) 
+                
+            })
+        }
