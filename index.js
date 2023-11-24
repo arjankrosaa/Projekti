00587b41dfa8a49cb2e3f6559400a8b5ce757959
arjankrosaa <149768322+arjankrosaa@users.noreply.github.com>
2023-11-24 09:50:21 +0100

    Projektet

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function llogaritDimensionet(value){
     rezultati= 300 * value
     document.getElementById("result").value=rezultati;
 }
+
+
+function NewTab() {
+            window.open("Projektet.html" , "_self");
+        }
