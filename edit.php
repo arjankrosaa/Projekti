0923544623b95980c654c84f55cbc84a405dccd2
arjankrosaa <149768322+arjankrosaa@users.noreply.github.com>
2024-01-22 13:04:39 +0100

    .

--- edit.php
+++ edit.php
@@ -23,16 +23,47 @@ $user  = $userRepository->getUserById($userId);
     <title>Document</title>
 </head>
 <body>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: white;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+
+
     <h3>Edit User</h3>
     <form action="" method="post">
         <input type="text" name="id"  value="<?=$user['id']?>" readonly> <br> <br>
-        <input type="text" name="name"  value="<?=$user['emri']?>"> <br> <br>
-        <input type="text" name="surname"  value="<?=$user['mbiemri']?>"> <br> <br>
+        <input type="text" name="emri"  value="<?=$user['emri']?>"> <br> <br>
+        <input type="text" name="mbiemri"  value="<?=$user['mbiemri']?>"> <br> <br>
         <input type="text" name="email"  value="<?=$user['email']?>"> <br> <br>
         <input type="text" name="password"  value="<?=$user['password']?>"> <br> <br>
 
         <input type="submit" name="editBtn" value="save"> <br> <br>
     </form>
+    <p><a href="dashboard.php">Back to Dashboard</a></p>
 </body>
 </html>
 
